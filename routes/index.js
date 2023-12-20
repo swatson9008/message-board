7ec9064cc8dfaccc8ea0963fa307cb9a03e38ddf
Summer <swatson9008@yahoo.com>
2023-12-20 12:24:01 -0500

    challenge complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+
+
+
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Message Board',
+    messages: messages,
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Submit a New Message' });
+});
+
+router.post('/new', function(req, res) {
+  try {
+    messages.push({
+      text: req.body.message,
+      user: req.body.author,
+      added: new Date(),
+    });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
